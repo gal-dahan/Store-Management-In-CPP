762e9da6b201fd53413dc65541673a92a1618e31
Gal Dahan <67056261+gal-dahan@users.noreply.github.com>
2021-06-26 11:06:51 +0300

    Add files via upload

---
+++ computer.h
@@ -0,0 +1,32 @@
+
+// computer.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CcomputerApp:
+// See computer.cpp for the implementation of this class
+//
+
+class CcomputerApp : public CWinApp
+{
+public:
+	CcomputerApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CcomputerApp theApp;
